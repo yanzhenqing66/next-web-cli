155b3969263a240c0be9f87ce33c7ed571c2940d
yzq <504767693@qq.com>
2022-03-27 17:47:12 +0800

    配置格式化代码

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+  * buildId - 字符串类型，构建的唯一标示
+  * dev - Boolean型，判断你是否在开发环境下
+  * isServer - Boolean 型，为true使用在服务端, 为false使用在客户端.
+  * defaultLoaders - 对象型 ，内部加载器, 你可以如下配置
+  *     babel - 对象型，配置 babel-loader.
+  *     hotSelfAccept - 对象型， hot-self-accept-loader配置选项.这个加载器只能用于高阶案例。如 @zeit/next-typescript添加顶层 typescript 页面。
+  */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      'components': path.resolve(__dirname, './components'),
+      'sass': path.resolve(__dirname, './sass'),
+      'static': path.resolve(__dirname, './static'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
