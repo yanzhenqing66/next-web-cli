f14b7561dcbbe1331fc04aa4adfdd166b6a9f5bb
yzq <504767693@qq.com>
2022-03-27 21:34:15 +0800

    feat: 配置环境变量

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.scss'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>hello world next.js</h1>
+      <h1>hello world next.js {process.env.NEXT_PUBLIC_CUSTOM_ENV}</h1>
     </div>
   )
 }
